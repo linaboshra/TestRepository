7bf932424314b3417efd2dd4c6a45f353aecf54c
linaboshra <linabotros@yahoo.com>
2020-01-27 11:15:10 -0500

    Sandbox.java

---
+++ Sandbox.java
@@ -0,0 +1,15 @@
+public class Sandbox {
+
+    public static void main(String[] args) {
+
+        double currentyear = 2020;
+        double yearborn = 2001;
+        
+        double age;
+
+        age = currentyear - yearborn;
+
+        System.out.println("Your age is " + age);
+    }
+
+}
